fe9dfadab5a0910c621b13bf4d0eab6bf5446c08
masud2005 <masud20012005@gmail.com>
2024-12-25 01:02:24 +0600

    Changed Default Theme and Added Loading Speaner

---
+++ src/components/Loading.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Loading = () => {
+    return (
+        <div className='min-h-[calc(100vh-321px)] flex justify-center items-center'>
+            <span className="loading loading-spinner loading-lg"></span>
+        </div>
+    );
+};
+
+export default Loading;
--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -1,13 +1,14 @@
 import React, { useContext } from 'react';
 import { Navigate, useLocation } from 'react-router-dom';
 import { AuthContext } from '../provider/AuthProvider';
+import Loading from '../components/Loading';
 
 const PrivateRoute = ({children}) => {
     const { user, loading } = useContext(AuthContext);
     const location = useLocation();
 
     if (loading) {
-        return <p className='text-center text-2xl my-10 font-bold'>Loading...</p>
+        return <Loading />
     }
 
     if (user) {
